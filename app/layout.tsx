71699f9f9996a7aaaeac6535141d71c8cec8458e
giacomoRanieri <jgiak92@gmail.com>
2025-11-07 12:00:18 +0100

    wip: graphql service

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { NuptiaeBeWrapper } from "@/lib/NuptiaeBeWrapper";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <NuptiaeBeWrapper>{children}</NuptiaeBeWrapper>
       </body>
     </html>
   );
---
+++ lib/NuptiaeBeWrapper.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloNextAppProvider,
+  InMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/client-integration-nextjs";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({ stripDefer: true }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export function NuptiaeBeWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
