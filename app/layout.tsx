ecf578f56a0b3488aaf6e6f567a4f4ed4820fd53
giacomoRanieri <jgiak92@gmail.com>
2025-12-03 17:20:18 +0100

    refactor: move  Wrapper component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { NuptiaeBeWrapper } from "@/lib/NuptiaeBeWrapper";
+import { NuptiaeBeWrapper } from "@/app/components/NuptiaeBeWrapper";
 import "./globals.css";
 
 const geistSans = Geist({
